0073dbf3de18c27eafb0224d6fddf9c4a7f4d727
Jojo <rueckwerts@bitbucket.org>
2016-11-07 12:00:53 +0100

    added basic auth support

--- create-api.sh
+++ create-api.sh
@@ -12,5 +12,10 @@ if [ ! -z "$API_PIWIK_ENDPOINT" ]; then
 		echo "adding piwik plugin: {$API_PIWIK_ENDPOINT}"
     curl -X PUT -H 'Accept: application/json, text/plain, */*' --data-binary '{"name":"piwik-log","config":{"piwik_endpoint":"'${API_PIWIK_ENDPOINT}'","timeout":10000,"keepalive":60000}}' --compressed ${KONG_HOST}:${KONG_ADMIN_PORT}/${API_NAME}/plugins
 fi
-
 echo "added kong api endpoint: ${API_PATH} -> ${API_UPSTREAM_URL}"
+
+
+if [ ! -z "$API_AUTH_USERNAME" ]; then
+    curl -X POST -s -d "username=${API_AUTH_USERNAME}" ${KONG_HOST}:${KONG_ADMIN_PORT}/consumers
+    curl -X PUT -s -d "username=${API_AUTH_USERNAME}" -d"password=${API_AUTH_PASSWORD}" ${KONG_HOST}:${KONG_ADMIN_PORT}/consumers/${API_AUTH_USERAME}/basic-auth
+    curl -X POST -s -d "name=basic-auth" ${KONG_HOST}:${KONG_ADMIN_PORT}/apis/${API_NAME}/plugins/
